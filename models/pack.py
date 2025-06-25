999a539823d0c85a1e4c1135faf2db14b862c8a1
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2025-06-25 12:07:36 +0000

    Se agrega boton de desempaquetado Rya

--- models/pack.py
+++ models/pack.py
@@ -7,3 +7,11 @@ class StockQuantPackage(models.Model):
     _inherit = 'stock.quant.package'
 
     move_line_ids = fields.One2many(comodel_name='stock.move.line',inverse_name='result_package_id',string='Operaciones')
+
+    def rya_unpack(self):
+        self.ensure_one()
+        if self.quant_ids:
+            self.unpack()
+        else:
+            for move_line in self.move_line_ids:
+                move_line.result_package_id = None
