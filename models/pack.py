3bb0e4c2a377118ceedc7a3cbf1fb508f67af0ea
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2025-06-26 18:55:17 +0000

    Se agrega agrupamiento cuando se desempaqueta

--- models/pack.py
+++ models/pack.py
@@ -13,5 +13,22 @@ class StockQuantPackage(models.Model):
         if self.quant_ids:
             self.unpack()
         else:
+            dict_products = {}
             for move_line in self.move_line_ids:
-                move_line.result_package_id = None
+                qty = move_line.quantity
+                if move_line.lot_id:
+                    move_line.result_package_id = None
+                elif not move_line.lot_id:
+                    domain = [
+                        ('picking_id','=',move_line.picking_id.id),
+                        ('result_package_id','=',False),
+                        ('id','!=',move_line.id),
+                        ('product_id','=',move_line.product_id.id),
+                        ('lot_id','=',None),
+                        ]
+                    other_move_line = self.env['stock.move.line'].search(domain,limit = 1)
+                    if other_move_line:
+                        other_move_line.quantity = other_move_line.quantity + move_line.quantity
+                        move_line.unlink()
+                    else:
+                        move_line.result_package_id = None
